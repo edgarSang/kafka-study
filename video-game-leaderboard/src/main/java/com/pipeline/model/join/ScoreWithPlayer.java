df9b0801a0e70088c097a23c41a448ddf235a148
SangHa.Han <sangha.han@linecorp.com>
2021-07-19 22:19:44 +0900

    add join model

---
+++ video-game-leaderboard/src/main/java/com/pipeline/model/join/ScoreWithPlayer.java
@@ -0,0 +1,27 @@
+package com.pipeline.model.join;
+
+import com.pipeline.model.Player;
+import com.pipeline.model.ScoreEvent;
+
+public class ScoreWithPlayer {
+    private ScoreEvent scoreEvent;
+    private Player player;
+
+    public ScoreWithPlayer(ScoreEvent scoreEvent, Player player) {
+        this.scoreEvent = scoreEvent;
+        this.player = player;
+    }
+
+    public ScoreEvent getScoreEvent() {
+        return this.scoreEvent;
+    }
+
+    public Player getPlayer() {
+        return this.player;
+    }
+
+    @Override
+    public String toString() {
+        return "{" + " scoreEvent='" + getScoreEvent() + "'" + ", player='" + getPlayer() + "'" + "}";
+    }
+}
